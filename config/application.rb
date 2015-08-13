1fe735b7cedf246799ebbd6914684e87a46475c5
Allam Marcos Campanini Matsubara <allam.matsubara@gmail.com>
2015-08-13 06:16:11 -0300

    Fixing deployment for a suburi.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module EventManager
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Brasilia'
     config.encoding = "utf-8"
+    config.relative_url_root = '/event_manager'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
