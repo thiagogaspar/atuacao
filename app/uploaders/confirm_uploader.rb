121e2eefc7d15c55438f8a46e5e0a3b31c9d73a8
Allam Marcos Campanini Matsubara <allam.matsubara@gmail.com>
2015-08-14 07:26:55 -0300

    New uploader

--- app/uploaders/confirm_uploader.rb
+++ app/uploaders/confirm_uploader.rb
@@ -11,7 +11,7 @@ class ConfirmUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "event_manager/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
